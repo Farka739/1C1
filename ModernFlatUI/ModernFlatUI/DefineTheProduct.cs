62f650c4024aaabb075306401421aef07b13e824
Anton Korotenko <71564658+Farka739@users.noreply.github.com>
2021-03-14 15:26:26 +0200

    Ne w

---
+++ ModernFlatUI/ModernFlatUI/DefineTheProduct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ModernFlatUI
+{
+    public partial class DefineTheProduct : Form
+    {
+        public string Path = Environment.CurrentDirectory + "\\ProductList.txt";
+        public DefineTheProduct()
+        {
+            InitializeComponent();
+        }
+
+        private string GetTheProduct()
+        {
+            
+            var productInfo = txtbID.Text + ' ' + txtbName.Text + ' ' + txtbPrice.Text + ' ' + nudQuantity.Text + ' ' + txtbDescription.Text;
+            return productInfo;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddTheProduct_Click(object sender, EventArgs e)
+        {
+            using (var productsList = File.AppendText(Path))
+            {
+                productsList.WriteLine(GetTheProduct());
+            }
+            
+        }
+    }
+}
---
+++ ModernFlatUI/ModernFlatUI/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModernFlatUI
+{
+    public partial class Form1 : Form
+    {
+        Form frmDefineTheProduct = new DefineTheProduct() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true};
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            CustomizeDesign();
+        }
+
+        private void CustomizeDesign()
+        {
+            pnlProductMaintenance.Visible = false;
+            pnlReports.Visible = false;
+        }
+
+        private void HideSubMenu()
+        {
+            if (pnlProductMaintenance.Visible == true)
+                pnlProductMaintenance.Visible = false;
+            if (pnlReports.Visible == true)
+                pnlReports.Visible = false;
+        }
+
+        private void ShowSubMenu(Control subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                HideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void ShowFormDefineTheProduct()
+        {
+
+            pnlMain.Controls.Add(frmDefineTheProduct);
+            frmDefineTheProduct.Show();
+        }
+
+        #region Reports
+        private void btnReports_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(pnlReports);
+        }
+
+        private void btnTotalSalesReport_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void btnTenMostSoldItems_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+        #endregion
+        
+        #region ProductMaintenance
+        private void btnDefineTheProduct_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+            ShowFormDefineTheProduct(); 
+        }
+
+        private void btnListOfProducts_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void btnProductMaintenance_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(pnlProductMaintenance);
+        }
+        #endregion
+
+        private void btnCashRegister_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+    }
+}
